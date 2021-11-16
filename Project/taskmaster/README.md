38bc948c2c8e6164bfad8fc0d0003671efaff31a
Vipul Tripathi <Vipul.Tripathi@neudesic.com>
2021-11-16 10:55:39 +0530

    Lernt render_template, url_for, adding css

---
+++ Project/taskmaster/README.md
@@ -0,0 +1,1 @@
+# Project for learning basics of flask
--- Project/taskmaster/app.py
+++ Project/taskmaster/app.py
@@ -1,15 +1,24 @@
 # Importing Flask from flask module
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 # initializing a flask application
-app = Flask("taskmaster")
+app = Flask(__name__)
+
+# dictionary to keep track of all templates
+templates = dict()
+
+templates["helloworld"] = "helloworld.html"
+templates["mainpage"] = "mainpage.html"
 
 # We need to route the user to somepage so that the application 
 # doesn't give any error 404 when immediately accessed by an user
 @app.route("/")
 def hello_world():
-    return "hello world"
+    # code to check what url_for function generates
+    url=url_for('static', filename='css/mainpage.css')
+    return render_template(templates["helloworld"], url=url)
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+    
